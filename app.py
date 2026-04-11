e8121b7700433e3daf53d12360891b73a8848788
Filippo Saccani <filippo.saccani.filippo@gmail.com>
2026-04-11 14:50:12 +0200

    fine navbar, fatto routing lato client, inizio pagina social media

--- app.py
+++ app.py
@@ -2,25 +2,13 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
-def root():  # put application's code here
-    return flask.redirect('/admin')
-
+@app.route('/admin')
 @app.route('/social')
-def social():
-    return flask.render_template('social.html')
-
-@app.route('/index')
-def index():
+def root():  # put application's code here
     return flask.render_template('index.html')
 
-@app.route('/admin')
-def admin():
-    return flask.render_template('admin.html')
-
-@app.route('/admin_old')
-def admin2():
-    return flask.render_template('admin_old.html')
 
 if __name__ == '__main__':
     app.run()
