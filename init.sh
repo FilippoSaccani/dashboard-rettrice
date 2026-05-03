fecee7bf33e9daea5456b5e6fe374e329c140950
Filippo Saccani <filippo.saccani.filippo@gmail.com>
2026-05-03 20:06:33 +0200

    fix install_ollama

--- init.sh
+++ init.sh
@@ -3,8 +3,6 @@ sudo systemctl start ollama
 
 sleep 3
 
-ollama pull gemma3
-
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
--- install_ollama.sh
+++ install_ollama.sh
@@ -1,1 +1,8 @@
 curl -fsSL https://ollama.com/install.sh | sh
+
+sudo systemctl enable ollama
+sudo systemctl start ollama
+
+sleep 3
+
+ollama pull gemma3
